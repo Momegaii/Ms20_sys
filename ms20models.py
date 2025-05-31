6c3f1daf37fa4b04754fa3e617226c1b4350c3ef
Mohammad <mohammadalmasri1053@gmail.com>
2025-06-01 02:15:22 +0300

    Update ms20models.py

--- ms20models.py
+++ ms20models.py
@@ -4,17 +4,17 @@ import os
 # Ensure the directory exists
 
 
-with open("E:/py.proj-20250522T154056Z-1-001/py.proj/MS_sys/model_100lb_6h.pkl", "rb")as f:
+with open("/model_100lb_6h.pkl", "rb")as f:
     modelcals100 = pickle.load(f)
 
-with open("E:/py.proj-20250522T154056Z-1-001/py.proj/MS_sys/model_200lb_6h.pkl", "rb")as f:
+with open("/model_200lb_6h.pkl", "rb")as f:
     modelcals200 = pickle.load(f)
 
-with open("E:/py.proj-20250522T154056Z-1-001/py.proj/MS_sys/model_300lb_6h.pkl", "rb")as f:
+with open("/model_300lb_6h.pkl", "rb")as f:
     modelcals300 = pickle.load(f)
-with open("lng_model.pkl","rb") as f:
+with open("/lng_model.pkl","rb") as f:
     lng_model = pickle.load(f)
-with open("E:/py.proj-20250522T154056Z-1-001/py.proj/MS_sys/diabetes_pred.pkl", "rb")as f:
+with open("/diabetes_pred.pkl", "rb")as f:
     daib_model = pickle.load(f)
 
 class MS20Models:
@@ -29,6 +29,3 @@ class MS20Models:
 models = MS20Models(modelcals100, modelcals200, modelcals300, lng_model, daib_model)
 
 
-
-pred = models.daib_model.predict([[33.4,198,8.9,1,1,1,1]])
-print(pred[0])
